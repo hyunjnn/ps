cef23032b8421f8195e06ca71ed9ebf07e0bc5ff
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-08-04 12:20:36 +0900

    Update 골드바흐 파티션.py

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,5 +1,4 @@
-from itertools import combinations
-
+# 에라토스테네스의 체 알고리즘 사용
 prime_num = [1 for _ in range(1000001)]
 for i in range(2, 1000001):
     if prime_num[i]:
@@ -8,14 +7,12 @@ for i in range(2, 1000001):
 
 t = int(input())
 
-
-for i in range(t):
+for _ in range(t):
     n = int(input())
     res = 0
+    # 절반 이상의 범위 부터는 두 값의 순서만 바뀌기 때문에 검사할 필요 없음
     for j in range(2, n // 2 + 1):
+        # 두 수의 합이 n 이면서 각각의 값이 소수일 때 카운트 증가
         if prime_num[j] and prime_num[n - j]:
             res += 1
     print(res)
-
-
-
