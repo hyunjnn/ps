974ba38dfaad6118ba013bba3fb125d261c6b342
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-11-23 15:30:37 +0900

    [Silver II] Title: 유기농 배추, Time: 152 ms, Memory: 114180 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,28 +1,31 @@
 from collections import deque
 
 def bfs(x, y, graph, visited):
-    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    # 갈 수 있는 방향(상, 하, 좌, 우)
+    directions = [(1, 0), (0, 1), (0, -1), (-1, 0)]
     queue = deque([(x, y)])
     visited[y][x] = True
     while queue:
         cx, cy = queue.popleft()
-        for dx, dy in directions:       
+        for dx, dy in directions:
             nx, ny = cx + dx, cy + dy
             if 0 <= nx and nx < len(graph[0]) and 0 <= ny and ny < len(graph) and not visited[ny][nx] and graph[ny][nx] == 1:
                 visited[ny][nx] = True
                 queue.append((nx, ny))
-            
-    
+
+
 T = int(input())
 for _ in range(T):
+    # 가로, 세로 길이, 배추 위치 개수
     M, N, K = map(int, input().split())
-    visited = [[False] * M for _ in range(N)]
+    # 배추 밭 초기화
     graph = [[0] * M for _ in range(N)]
-    
+    visited = [[False] * M for _ in range(N)]
+    # 배추 표시
     for _ in range(K):
         X, Y = map(int, input().split())
         graph[Y][X] = 1
-        
+    # 필요한 지렁이 계산
     worm = 0
     for y in range(N):
         for x in range(M):
@@ -30,8 +33,5 @@ for _ in range(T):
                 bfs(x, y, graph, visited)
                 worm += 1
     print(worm)
-        
-    
-    
     
     
