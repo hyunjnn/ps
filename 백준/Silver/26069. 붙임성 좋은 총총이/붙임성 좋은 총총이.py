4eca3d0299bc010da973651f87ef49101f9557a1
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-11-29 17:27:16 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 104 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -1,14 +1,8 @@
 N = int(input())
 names = {"ChongChong"}
-count = 1
 for _ in range(N):
-    arr = list(input().split())
-    if all(x in names for x in arr):
-        continue
-    if any(x in names for x in arr):
-        if arr[0] in names:
-            names.add(arr[1])
-        else:
-            names.add(arr[0])
-        count += 1 
-print(count)
+    a, b = input().split()
+    if a in names or b in names:
+        names.add(a)
+        names.add(b)
+print(len(names))
