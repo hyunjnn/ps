dca779c7bd1e74fa34a8ad4bfa96b41b5a435668
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-11-29 23:35:49 +0900

    [Silver III] Title: N과 M (1), Time: 124 ms, Memory: 110800 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,6 +1,8 @@
 from itertools import permutations
 
 N, M = map(int, input().split())
-arr = [i for i in range(1, N + 1)]
-for x in permutations(arr, M):
-    print(" ".join(list(map(str, x))))
+nums = [i for i in range(1, N + 1)]
+
+res = permutations(nums, M)
+for x in res:
+    print(str(x).replace("(", "").replace(")", "").replace(",", ""))
