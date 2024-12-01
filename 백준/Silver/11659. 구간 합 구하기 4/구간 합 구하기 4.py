8fea4e2271b1d9058df13bb396e7b15063c69c84
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-12-01 22:26:46 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 188 ms, Memory: 122484 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,5 +1,7 @@
-N, M = map(int, input().split())
-nums = list(map(int, input().split()))
+import sys
+
+N, M = map(int, sys.stdin.readline().strip().split())
+nums = list(map(int, sys.stdin.readline().strip().split()))
 
 # 누적합 계산
 prefix = [0] * N
@@ -8,9 +10,6 @@ for i in range(1, N):
     prefix[i] = prefix[i - 1] + nums[i]
     
 for _ in range(M):
-    a, b = map(int, input().split())
-    if a == b:
-        print(nums[a - 1])
-        continue
+    a, b = map(int, sys.stdin.readline().strip().split())
     # 누적합으로 구간합 계산
     print(prefix[b - 1] - (prefix[a - 2] if a > 1 else 0))
