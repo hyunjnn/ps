24197079be480a98f20c16a077f83d88f1f199cc
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-12-02 19:41:26 +0900

    [Silver III] Title: 2×n 타일링, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,7 +1,13 @@
 n = int(input())
-dp = [0] * 1001
-dp[1] = 1
-dp[2] = 2
-for i in range(3, n + 1):
-    dp[i] = dp[i-1] + dp[i-2]
-print(dp[n] % 10007)
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
+else:
+    prev2 = 1
+    prev1 = 2
+    for _ in range(3, n + 1):
+        current = (prev2 + prev1) % 10007
+        prev2 = prev1
+        prev1 = current
+    print(current)    
