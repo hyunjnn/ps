b0e646c297326d347ce7f68a93275e7bde471aa9
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-12-15 13:43:23 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,2 @@
 def solution(array, commands):
-    answer = []
-    for i, j, k in commands:
-        sorted_arr = sorted(array[i-1:j])
-        answer.append(sorted_arr[k-1])
-    return answer
+    return list(map(lambda x: sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
