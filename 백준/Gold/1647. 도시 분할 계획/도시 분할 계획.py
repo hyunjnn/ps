401fd10a7adbc7685dda7eaefd090af4744ed972
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-12-23 12:10:34 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 6592 ms, Memory: 257996 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,32 @@
+from heapq import heappush, heappop
+
+N, M = map(int, input().split())
+edges = []
+for _ in range(M):
+    A, B, C = map(int, input().split())
+    heappush(edges, (C, A, B))
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+    
+def union(parent, x, y):
+    x = find(parent, x)
+    y = find(parent, y)
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+
+parent = [i for i in range(N + 1)]
+res_edges = []
+# 최소 유지비의 합
+res = 0
+while edges:
+    cost, a, b = heappop(edges)
+    if find(parent, a) != find(parent, b):
+        union(parent, a, b)
+        res += cost
+        heappush(res_edges, -cost)
+print(res - (-heappop(res_edges)))
