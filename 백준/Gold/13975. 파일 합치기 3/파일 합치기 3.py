151ba16b3645fbe28e0f4beb2a2d7392a4db3a12
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-01-01 16:55:35 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 2052 ms, Memory: 348216 KB -BaekjoonHub

--- 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.py
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.py
@@ -1,12 +1,13 @@
 import sys
 from heapq import heappush, heappop, heapify
-input = sys.stdin.readline
+input = sys.stdin.read
+data = input().split()
 
-T = int(input())
-for _ in range(T):
-    
-    K = int(input())
-    files = list(map(int, input().split()))
+i = 1
+for _ in range(int(data[0])):
+    K = int(data[i])
+    files = list(map(int, data[i + 1 : K + i + 1]))
+    i += K + 1
     heapify(files)
     
     min_val = 0
