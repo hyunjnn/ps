bf87a16d506c811031f60ffcf258e498d2b4c1cb
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-01-15 16:11:24 +0900

    [Gold III] Title: League of Overwatch at Moloco (Hard), Time: 2112 ms, Memory: 241476 KB -BaekjoonHub

---
+++ 백준/Gold/15511. League of Overwatch at Moloco （Hard）/League of Overwatch at Moloco （Hard）.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def is_bipartite(graph, n):
+    tags = [0] * (n + 1)
+    # 비연결 그래프를 고려한 모든 노드 탐색
+    for start in range(1, n + 1):
+        if tags[start] != 0: continue
+        q = deque([start])
+        tags[start] = 1
+        while q:
+            curr = q.popleft()
+            for adj in graph[curr]:
+                if tags[adj] == 0:
+                    tags[adj] = -tags[curr]
+                    q.append(adj)
+                elif tags[adj] == tags[curr]:
+                    return False
+    return True
+    
+
+V, E = map(int, input().split())
+
+graph = [[] for _ in range(V + 1)]
+for _ in range(E):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+print("POSSIBLE" if is_bipartite(graph, V) else "IMPOSSIBLE")
