208bfaad89e580885706f86858f533686ab208ea
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-01-16 18:35:58 +0900

    [Gold II] Title: 책 나눠주기, Time: 184 ms, Memory: 20252 KB -BaekjoonHub

--- 백준/Gold/9576. 책 나눠주기/책 나눠주기.java
+++ 백준/Gold/9576. 책 나눠주기/책 나눠주기.java
@@ -1,65 +1,44 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-    static boolean[] visited;
-    static HashMap<Integer, Integer> matched;
-    static List<List<Integer>> preferredBooks;
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         int T = Integer.parseInt(st.nextToken());
+        
         for (int t = 0; t < T; t++) {
             st = new StringTokenizer(br.readLine());
-            int bookCount = Integer.parseInt(st.nextToken());
-            int studentCount = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
             
-            preferredBooks = new ArrayList<>();
-            for (int i = 0; i < studentCount + 1; i++) {
-                preferredBooks.add(new ArrayList<>());
-            }
-            matched = new HashMap<>();
-            for (int book = 1; book < bookCount + 1; book++) {
-                matched.put(book, 0);
-            }
-            
-            for (int student = 1; student <= studentCount; student++) {
+            int[][] requests = new int[M][2];
+            for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
-                int n1 = Integer.parseInt(st.nextToken());
-                int n2 = Integer.parseInt(st.nextToken());
-                for (int book = n1; book <= n2; book++) {
-                    preferredBooks.get(student).add(book);
-                }
+                requests[i][0] = Integer.parseInt(st.nextToken());
+                requests[i][1] = Integer.parseInt(st.nextToken());
             }
+            Arrays.sort(requests, (a, b) -> Integer.compare(a[1], b[1]));
             
-            int maxStudentCount = 0;
-            for (int student = 1; student <= studentCount; student++) {
-                visited = new boolean[bookCount + 1];
-                if (tryMatching(student)) {
-                    maxStudentCount++;
+            boolean[] used = new boolean[N + 1];
+            int maxStudents = 0;
+            
+            for (int[] request: requests) {
+                int start = request[0];
+                int end = request[1];
+                for (int book = start; book <= end; book++) {
+                    if (!used[book]) {
+                        maxStudents++;
+                        used[book] = true;
+                        break;
+                    }
                 }
             }
-            System.out.println(maxStudentCount);
+            System.out.println(maxStudents);
         }
-    }
-    
-    static boolean tryMatching(int student) {
-        for (int book: preferredBooks.get(student)) {
-            if (!visited[book]) {
-                visited[book] = true;
-                if (matched.get(book) == 0 || tryMatching(matched.get(book))) {
-                    matched.put(book, student);
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 }
