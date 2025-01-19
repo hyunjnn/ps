71b2cc38997bf2b901c7b3f5fab11f197ab2d0ff
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-01-19 16:52:07 +0900

    [Gold IV] Title: 미로만들기, Time: 124 ms, Memory: 114416 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -5,31 +5,27 @@ input = sys.stdin.readline
 DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 INF = int(1e9)
 
-
 def count_black(graph, n, start_x, start_y):
     costs = [[INF] * n for _ in range(n)]
     
-    costs[start_x][start_y] = True
-    dq = deque([(0, start_x, start_y)])
+    costs[start_x][start_y] = 0
+    q = deque([(start_x, start_y)])
     
-    while dq:
-        cost, x, y = dq.popleft()
-        if cost > costs[x][y]:
-            continue
+    while q:
+        x, y = q.popleft()
         
         for dx, dy in DIRECTIONS:
             nx, ny = x + dx, y + dy
             
             if 0 <= nx < n and 0 <= ny < n:
-                new_cost = cost + (1 if graph[nx][ny] == 0 else 0)
-                
-                if new_cost < costs[nx][ny]:
-                    costs[nx][ny] = new_cost
-                    
-                    if graph[nx][ny] == 0:  # black
-                        dq.append((new_cost, nx, ny))
-                    else:
-                        dq.appendleft((new_cost, nx, ny))
+                # black
+                if graph[nx][ny] == 0 and costs[nx][ny] > costs[x][y] + 1:  
+                    q.append((nx, ny))
+                    costs[nx][ny] = costs[x][y] + 1
+                # white
+                elif graph[nx][ny] == 1 and costs[nx][ny] > costs[x][y]:
+                    q.appendleft((nx, ny))
+                    costs[nx][ny] = costs[x][y]
                     
     return costs[n - 1][n - 1]
 
