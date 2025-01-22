478ebe062ca5b4f534b9fc053156596f72756a11
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-01-22 16:30:52 +0900

    [Silver III] Title: N과 M (2), Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,6 +1,23 @@
-from itertools import combinations
+import sys
+input = sys.stdin.readline
 
-N, M = map(int, input().split())
-nums = [i for i in range(1, N + 1)]
-for x in combinations(nums, M):
-    print(" ".join(list(map(str, x))))
+def main():
+    max_num, target_length = map(int, input().split())
+    cur_sequence = []
+    
+    def generate_sequence(level):
+        if level == target_length:
+            print(" ".join(map(str, cur_sequence)))
+        for n in range(1, max_num + 1):
+            if cur_sequence and n <= cur_sequence[-1]:
+                continue
+            cur_sequence.append(n)
+            generate_sequence(level + 1)
+            cur_sequence.pop()
+            
+            
+    generate_sequence(0)
+    
+
+if __name__ == "__main__":
+    main()
