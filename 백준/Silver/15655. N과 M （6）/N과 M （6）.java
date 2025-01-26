2cf4f05dbc6ba3caf5b9f79256412f1376b41afb
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-01-26 14:05:28 +0900

    [Silver III] Title: N과 M (6), Time: 108 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Main {
+    static int N, M;
+    static int[] nums;
+    static ArrayList<Integer> sequence = new ArrayList<>();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(nums);
+        backtrack(0);
+    }
+    
+    static void backtrack(int start) {
+        if (sequence.size() == M) {
+            StringBuilder sb = new StringBuilder();
+            for (int n: sequence) {
+                sb.append(n).append(" ");
+            }
+            System.out.println(sb.toString().trim());
+            return;
+        }
+        
+        for (int i = start; i < N; i++) {
+            sequence.add(nums[i]);
+            backtrack(i + 1);
+            sequence.remove(sequence.size() - 1);
+        }
+    }
+}
