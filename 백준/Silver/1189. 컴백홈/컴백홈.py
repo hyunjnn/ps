3a6f4d6e679de506753db8314069784ac65030ec
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-01-31 13:22:54 +0900

    [Silver I] Title: 컴백홈, Time: 172 ms, Memory: 112112 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.py
+++ 백준/Silver/1189. 컴백홈/컴백홈.py
@@ -6,25 +6,20 @@ R, C, K = map(int, input().split())
 board = [list(input().strip()) for _ in range(R)]
 
 visited = [[False] * C for _ in range(R)]
-
-obstacle = []
-for i in range(R):
-    for j in range(C):
-        if board[i][j] == "T":
-            obstacle.append((i, j))
             
-
 def backtrack(cx, cy, level):
     global count
     
-    if cx == 0 and cy == C - 1 and level == K:
-        count += 1
+    if cx == 0 and cy == C - 1:
+        if level == K:
+            count += 1
         return
     
     for dx, dy in DIRECTIONS:
-        nx, ny = dx + cx, dy + cy
+        nx, ny = cx+dx, cy+dy
         if 0<=nx<R and 0<=ny<C and not visited[nx][ny]:
-            if board[nx][ny] == "T": continue
+            if board[nx][ny] == "T": 
+                continue
             visited[nx][ny] = True
             backtrack(nx, ny, level+1)
             visited[nx][ny] = False
