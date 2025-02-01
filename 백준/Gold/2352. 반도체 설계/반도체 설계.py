c52a8375d9abc464afafc4bb2dd68bd58023d3ab
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-02-01 14:56:11 +0900

    [Gold II] Title: 반도체 설계, Time: 116 ms, Memory: 114780 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.py
@@ -0,0 +1,14 @@
+from bisect import bisect_left
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+res = []
+for n in nums:
+    i = bisect_left(res, n)
+    if i == len(res):
+        res.append(n)
+    else:
+        res[i] = n
+
+print(len(res))
