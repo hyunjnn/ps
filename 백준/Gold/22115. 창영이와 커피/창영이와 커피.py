3f8d0f0182e672f7dbe1614a0e449ea596f78737
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-02-10 14:00:08 +0900

    [Gold V] Title: 창영이와 커피, Time: 156 ms, Memory: 111112 KB -BaekjoonHub

--- 백준/Gold/22115. 창영이와 커피/창영이와 커피.py
+++ 백준/Gold/22115. 창영이와 커피/창영이와 커피.py
@@ -2,17 +2,14 @@ import sys
 imput = sys.stdin.readline
 
 N, K = map(int, input().split())
-C = list(map(int, input().split()))
-# i번 커피까지 확인했을 때, 카페인 양이 k가 되기 위한 최소 커피 개수
-dp = [[1e9]*(K+1) for _ in range(N+1)]
-for i in range(N+1):
-    dp[i][0] = 0
+C = map(int, input().split())
 
-for i in range(1, N+1):
-    for c in range(K+1):
-        if c < C[i-1]:
-            dp[i][c] = dp[i-1][c]
-        else:
-            dp[i][c] = min(dp[i-1][c], dp[i-1][c-C[i-1]] + 1)
+# 카페인 양이 k가 되기 위한 최소 커피 개수
+dp = [1e9]*(K+1)
+dp[0] = 0
+
+for c in C:
+    for i in range(K, c-1, -1):
+        dp[i] = min(dp[i], dp[i-c]+1)
         
-print(dp[N][K] if dp[N][K] != 1e9 else -1)
+print(dp[K] if dp[K] != 1e9 else -1)
